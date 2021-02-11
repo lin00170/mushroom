101b6890e9e5df9c6a112a48e3e8f839facb8a2c
linsi <lin00170@algonquinlive.com>
2021-02-11 12:53:55 -0500

    a

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+//Material section
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatInputModule} from '@angular/material/input';
+import {MatTreeModule} from '@angular/material/tree';
+
+import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+
+import {MessagesListComponent} from './messages/messages-list/messages-list.component'
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    //Material
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatInputModule,
+    MatTreeModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap:[AppComponent]
+})
+export class AppModule { }
