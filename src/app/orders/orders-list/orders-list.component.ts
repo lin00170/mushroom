6e0a116e3c5ca7481fd49014c7c784cfafe16eb2
linsi <lin00170@algonquinlive.com>
2021-02-12 11:28:00 -0500

    'secondversion'

--- src/app/orders/orders-list/orders-list.component.ts
+++ src/app/orders/orders-list/orders-list.component.ts
@@ -7,44 +7,14 @@ import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
  * Food data with nested structure.
  * Each node has a name and an optional list of children.
  */
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
+interface detailCard {
+  title: string;
+  subtitle: string;
+  price:number;
+  commit: string;
+  imgUrl:string;
 }
 
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [
-      {name: 'Apple'},
-      {name: 'Banana'},
-      {name: 'Fruit loops'},
-    ]
-  }, {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [
-          {name: 'Broccoli'},
-          {name: 'Brussels sprouts'},
-        ]
-      }, {
-        name: 'Orange',
-        children: [
-          {name: 'Pumpkins'},
-          {name: 'Carrots'},
-        ]
-      },
-    ]
-  },
-];
-/** Flat node with expandable and level information */
-interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-}
 
 /**
  * @title Tree with flat nodes
@@ -54,29 +24,34 @@ interface ExampleFlatNode {
   templateUrl: './orders-list.component.html',
   styleUrls: ['./orders-list.component.css']
 })
+
 export class OrdersListComponent implements OnInit {
   options:string[]=['from low to high','from high to low'];
-  private _transformer = (node: FoodNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-    };
-  }
-
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level, node => node.expandable);
-
-  treeFlattener = new MatTreeFlattener(
-    this._transformer, node => node.level, node => node.expandable, node => node.children);
-
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+  
+  /** Flat node with expandable and level information */
+details:detailCard[]=[
+  {title:"lion's mane",
+  subtitle:"fresh mushroom",
+  price:200,
+  commit: "",
+  imgUrl:"https://s3.amazonaws.com/cdn.fieldforest.net/images/thumb/lions-mane-table-top-farm-1.jpg"},
+  {title:"lion's mane",
+  subtitle:"fresh mushroom",
+  price:200,
+  commit: "",
+  imgUrl:"https://s3.amazonaws.com/cdn.fieldforest.net/images/thumb/shiitake-table-top-farm-1.jpg"},
+  {title:"lion's mane",
+  subtitle:"fresh mushroom",
+  price:200,
+  commit: "",
+  imgUrl:"https://s3.amazonaws.com/cdn.fieldforest.net/images/thumb/lions-mane-table-top-farm-1.jpg"}
+]
 
   constructor() {
-    this.dataSource.data = TREE_DATA;
+  
   }
 
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
 
   ngOnInit(): void {
   }
