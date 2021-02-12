af5980e6ac5085c552c06d226bb88f11c5ac6cb1
linsi <lin00170@algonquinlive.com>
2021-02-11 20:53:51 -0500

    'mushroomfirstversion'

--- src/app/swiper/swiper.component.ts
+++ src/app/swiper/swiper.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 // import Swiper core and required components
-import SwiperCore,{Navigation, Pagination, Scrollbar, A11y,Autoplay,EffectCube} from 'swiper/core';
+import SwiperCore,{Navigation, Pagination, Scrollbar, A11y,Autoplay,EffectCube, EffectFlip} from 'swiper/core';
 
-SwiperCore.use([Navigation, Pagination, Scrollbar,A11y,EffectCube,Autoplay]);
+SwiperCore.use([Navigation, Pagination, Scrollbar,A11y,EffectCube,Autoplay,EffectFlip]);
 
 @Component({
   selector: 'app-swiper',
